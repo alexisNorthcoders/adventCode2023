132cb1f266e2ad7e58d35c118f61e5f2177a61c0
AlexisNorthCoders <alexiscreoulo@hotmail.com>
2023-12-18 00:09:17 +0000

    Day 5 Part 1 solution Advent of Code 2023

--- day5part1.js
+++ day5part1.js
@@ -5,7 +5,7 @@ fs.readFile('inputday5.txt', 'utf8', (err, data) => {
       return;
     }
     const result = fertilizer(data);
-    console.log("Total scratch points:", result); 
+    console.log("Lowest Location:", result); 
 });
 
 function fertilizer(lines){
@@ -20,41 +20,24 @@ function fertilizer(lines){
     const lightToTemperature = parsedText[5].slice(0,-2).map(element => element.split(" "))
     const temperatureToHumidity = parsedText[6].slice(0,-2).map(element => element.split(" "))
     const humidityToLocation = parsedText[7].map(element => element.split(" "))
-    
-    function mapTo(array) {
-        const mapToObj = {}
-        for (let i = 0; i < array.length; i++) {
-            let destination = +array[i][0]
-            let source = +array[i][1]
-            let length = +array[i][2]
-            let counter = 0
-            for (let j = source; j < source+length; j++) {
-                mapToObj[source+counter]=destination+counter
-                counter++
-            }
-        }
-        return mapToObj
-    }
-const seedToSoilObj = mapTo(seedToSoil)
-const soilToFertilizerObj = mapTo(soilToFertilizer)
-const fertilizerToWaterObj = mapTo(fertilizerToWater)
-const waterToLightObj = mapTo(waterToLight)
-const lightToTemperatureObj = mapTo(lightToTemperature)
-const temperatureToHumidityObj = mapTo(temperatureToHumidity)
-const humidityToLocationObj = mapTo(humidityToLocation)
 
-const allMapsArray = [seedToSoilObj,soilToFertilizerObj,fertilizerToWaterObj,waterToLightObj,
-    lightToTemperatureObj,temperatureToHumidityObj,humidityToLocationObj]
-const seedFinalDestination = []
+    const seedFinalDestination = [] 
+    const mapsArray = [seedToSoil, soilToFertilizer, fertilizerToWater,waterToLight,
+        lightToTemperature,temperatureToHumidity,humidityToLocation]
 
-for (let i = 0; i < seeds.length; i++) {
-    let tempCoords=seeds[i]
-    for (let j = 0; j < allMapsArray.length; j++) {
-        tempCoords = (allMapsArray[j][tempCoords]) ? allMapsArray[j][tempCoords] : tempCoords
-        
+    for (let i = 0; i < seeds.length; i++) {
+        let tempCoords = Number(seeds[i])
+        for (let j = 0; j < mapsArray.length; j++) {
+            for (let k = 0; k< mapsArray[j].length;k++){
+                let subArray = mapsArray[j][k]
+                if (tempCoords >= Number(subArray[1]) && tempCoords <= Number(subArray[1])+Number(subArray[2]))  { 
+                    tempCoords = Number(subArray[0]) - Number(subArray[1]) + tempCoords
+                    k = mapsArray[j].length
+                 }
+               }
+           }
+        seedFinalDestination.push(tempCoords)
     }
-    seedFinalDestination.push(tempCoords)
-    
-}
-console.log(seedFinalDestination)
+    seedFinalDestination.sort((a, b) => a - b)
+    return seedFinalDestination[0]
 }
