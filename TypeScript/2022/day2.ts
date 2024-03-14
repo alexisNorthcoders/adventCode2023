b17baf64a3254d854bb4abbb2cec01798540177d
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-14 23:37:53 +0000

    add solution to part 2 day 2

--- TypeScript/2022/day2.ts
+++ TypeScript/2022/day2.ts
@@ -26,10 +26,32 @@ function totalScore(input: string[][]) {
                 else if (game[1] === "Y") total += 0 + 2
                 else if (game[1] === "X") total += 6 + 1
                 break
-
+        }
+    })
+    return total
+}function alternateTotalScore(input: string[][]) {
+    let total = 0
+    input.forEach((game) => {
+        switch (game[0]) {
+            case "A":
+                if (game[1] === "X") total += 0 + 3
+                else if (game[1] === "Y") total += 3 + 1
+                else if (game[1] === "Z") total += 6 + 2
+                break
+            case "B":
+                if (game[1] === "X") total += 0 + 1
+                else if (game[1] === "Y") total += 3 + 2
+                else if (game[1] === "Z") total += 6 + 3
+                break
+            case "C":
+                if (game[1] === "X") total += 0 + 2
+                else if (game[1] === "Y") total += 3 + 3
+                else if (game[1] === "Z") total += 6 + 1
+                break
         }
     })
     return total
 }
 
 console.log("Total Score: ", totalScore(games))
+console.log("Total Score Rules v2.0: ", alternateTotalScore(games))
