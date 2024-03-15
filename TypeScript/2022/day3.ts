5aa35036aa644b2cb894647786bbbbedf6032630
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-15 12:06:01 +0000

    add Sets to find intersection, add final solution for part 1 day 3

--- TypeScript/2022/day3.ts
+++ TypeScript/2022/day3.ts
@@ -16,14 +16,26 @@ lines.forEach((line: string) => {
     compartments.first.push(line.substring(0, line.length / 2))
     compartments.second.push(line.substring(line.length / 2, line.length))
 })
-function findSameItem(input: Compartments) {
-
+function findSameItemAndSumPriorities(rucksacks: Compartments):number {
+    let total = 0
+    rucksacks.first.forEach((rucksack,index)=>{
+        const firstSet:any = new Set(rucksack.split(""))
+        const secondSet = new Set(rucksacks.second[index].split(""))
+        const commonItem = firstSet.intersection(secondSet)
+        total += getPriorityValue(commonItem)
+        
+    })
+    return total
 }
-function getPriorityValue(item: string) {
-if (item.toLocaleLowerCase() === item) {
-        return item.charCodeAt(0) - 96
+function getPriorityValue(item: Set<string>):number {
+    const value:string = item.values().next().value
+    const charCode:number = value.charCodeAt(0)
+    if (value.toLocaleLowerCase() === value) {
+        return charCode - 96
     }
     else {
-        return item.charCodeAt(0) - 38
+        return charCode - 38
     }
 }
+
+console.log(findSameItemAndSumPriorities(compartments))
