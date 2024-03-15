7f532af96ad2f3d3c3873083362281756e60d517
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-15 11:41:48 +0000

    add function to get priority value

--- TypeScript/2022/day3.ts
+++ TypeScript/2022/day3.ts
@@ -3,3 +3,27 @@ const file = Bun.file(path);
 
 const text: string = await file.text();
 const lines: string[] = text.split("\n")
+
+interface Compartments {
+    first: string[];
+    second: string[];
+}
+const compartments: Compartments = {
+    first: [],
+    second: []
+}
+lines.forEach((line: string) => {
+    compartments.first.push(line.substring(0, line.length / 2))
+    compartments.second.push(line.substring(line.length / 2, line.length))
+})
+function findSameItem(input: Compartments) {
+
+}
+function getPriorityValue(item: string) {
+if (item.toLocaleLowerCase() === item) {
+        return item.charCodeAt(0) - 96
+    }
+    else {
+        return item.charCodeAt(0) - 38
+    }
+}
