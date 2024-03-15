2dddb0ec4a1b8393f5abe847b291f53c00fea615
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-15 21:27:22 +0000

    add part 2 day 6 solution

--- TypeScript/2022/day6.ts
+++ TypeScript/2022/day6.ts
@@ -13,5 +13,16 @@ function findFirstMarker(input: string) {
     }
     return marker + 4
 }
+function findFirstMessageMarker(input: string) {
+    let marker: number = 0
+    for (let i = 0; i < input.length; i++) {
+        if (new Set(text.slice(0 + i, 14 + i).split("")).size === 14) {
+            marker = i
+            break
+        }
+    }
+    return marker + 14
+}
 
 console.log("First Marker: ", findFirstMarker(text))
+console.log("First Message Marker: ", findFirstMessageMarker(text))
