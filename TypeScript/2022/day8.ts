95d13d2799b44855b212d4f376db9c91747fed06
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-17 11:21:15 +0000

    count outer edge trees

--- TypeScript/2022/day8.ts
+++ TypeScript/2022/day8.ts
@@ -3,3 +3,18 @@ const file = Bun.file(path);
 
 const text: string = await file.text();
 const lines: string[] = text.split("\n")
+
+function countVisibleTrees(input:string[]){
+    let count = 0
+input.forEach((treeLine,index)=>{
+    if (index===0 || index===treeLine.length-1){
+count += treeLine.length 
+    }
+    else{
+        count +=2
+    }
+})
+return count
+}
+
+console.log("Visible trees: ",countVisibleTrees(lines))
