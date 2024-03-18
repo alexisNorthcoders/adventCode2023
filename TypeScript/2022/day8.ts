737d4a8fcdc4e97b25f03125c66ce2691ede47cc
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-18 11:13:37 +0000

    add arrays to map the top left bottom and right trees

--- TypeScript/2022/day8.ts
+++ TypeScript/2022/day8.ts
@@ -4,17 +4,39 @@ const file = Bun.file(path);
 const text: string = await file.text();
 const lines: string[] = text.split("\n")
 
-function countVisibleTrees(input:string[]){
+function countVisibleTrees(input: string[]) {
+    const topEdge: number[] = []
+    const leftEdge: number[] = []
+    const rightEdge: number[] = []
+    const bottomEdge: number[] = []
     let count = 0
-input.forEach((treeLine,index)=>{
-    if (index===0 || index===treeLine.length-1){
-count += treeLine.length 
-    }
-    else{
-        count +=2
-    }
-})
-return count
+    input.forEach((treeLine: string, index: number) => {
+        if (index === 0) {
+            count += treeLine.length
+            //topEdge.push(...treeLine.slice(1,-1).split("").map(Number))
+
+            for (let i = 1; i < treeLine.length - 1; i++) {
+                topEdge.push(Number(treeLine[i]));
+            }
+        }
+        else if (index === treeLine.length - 1) {
+
+            for (let i = 1; i < treeLine.length - 1; i++) {
+                bottomEdge.push(Number(treeLine[i]));
+            }
+            count += treeLine.length
+        }
+        else {
+            leftEdge.push(Number(treeLine[0]))
+            rightEdge.push(Number(treeLine.slice(-1)))
+            count += 2
+        }
+    })
+    console.log(topEdge)
+    console.log(bottomEdge)
+    console.log(leftEdge)
+    console.log(rightEdge)
+    return count
 }
 
-console.log("Visible trees: ",countVisibleTrees(lines))
+console.log("Visible trees: ", countVisibleTrees(lines))
