f4e561c780b0bb2940f23745f2c6dab16aeb67f1
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-23 11:14:26 +0000

    add sample 9 data, add initial Class for Head

--- TypeScript/2022/day9.ts
+++ TypeScript/2022/day9.ts
@@ -3,3 +3,36 @@ const file = Bun.file(path);
 
 const text: string = await file.text();
 const lines: string[] = text.split("\n")
+
+console.log(lines)
+
+class Head {
+ startingX: number
+    startingY: number
+    constructor(startingX =0, startingY =0) {
+       
+        this.startingX = startingX
+        this.startingY = startingY
+    }
+    move(direction: string, steps: number) {
+        switch (direction) {
+            case "U":
+                this.startingY += steps;
+                break;
+            case "D":
+                this.startingY -= steps;
+                break;
+            case "L":
+                this.startingX -= steps;
+                break;
+            case "R":
+                this.startingX += steps;
+                break;
+}
+return {x:this.startingX,y:this.startingY}
+    }
+
+}
+
+const head = new Head()
+console.log(head.move("R",2))
