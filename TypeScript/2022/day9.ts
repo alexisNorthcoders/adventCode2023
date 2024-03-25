cb6f19f26dd6558f0fd6663a787c2834a431a6bd
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-25 22:58:20 +0000

    add solution for part 1 day 9

--- TypeScript/2022/day9.ts
+++ TypeScript/2022/day9.ts
@@ -4,35 +4,86 @@ const file = Bun.file(path);
 const text: string = await file.text();
 const lines: string[] = text.split("\n")
 
-console.log(lines)
-
 class Head {
- startingX: number
-    startingY: number
-    constructor(startingX =0, startingY =0) {
-       
-        this.startingX = startingX
-        this.startingY = startingY
+    headX: number
+    headY: number
+    tailX: number
+    tailY: number
+    uniquePositions: Set<string>
+    constructor(headX = 0, headY = 0, tailX = 0, tailY = 0) {
+
+        this.headX = headX
+        this.headY = headY
+        this.tailX = tailX
+        this.tailY = tailY
+        this.uniquePositions = new Set()
+        this.tailCurrentPosition()
     }
     move(direction: string, steps: number) {
         switch (direction) {
             case "U":
-                this.startingY += steps;
+                this.headY += steps;
+                for (let i = 0; i < steps; i++) {
+                    if (Math.hypot(this.headX - this.tailX, this.headY - this.tailY) > Math.SQRT2) {
+                        this.tailX = this.headX
+                        this.tailY++
+                        this.tailCurrentPosition()
+                    }
+                }
                 break;
             case "D":
-                this.startingY -= steps;
+                this.headY -= steps;
+                for (let i = 0; i < steps; i++) {
+                    if (Math.hypot(this.headX - this.tailX, this.headY - this.tailY) > Math.SQRT2) {
+                        this.tailX = this.headX
+                        this.tailY -= 1
+                        this.tailCurrentPosition()
+                    }
+                }
                 break;
             case "L":
-                this.startingX -= steps;
+                this.headX -= steps;
+                for (let i = 0; i < steps; i++) {
+                    if (Math.hypot(this.headX - this.tailX, this.headY - this.tailY) > Math.SQRT2) {
+                        this.tailY = this.headY
+                        this.tailX -= 1
+                        this.tailCurrentPosition()
+                    }
+                }
                 break;
             case "R":
-                this.startingX += steps;
+                this.headX += steps;
+                for (let i = 0; i < steps; i++) {
+                    if (Math.hypot(this.headX - this.tailX, this.headY - this.tailY) > Math.SQRT2) {
+                        this.tailY = this.headY
+                        this.tailX += 1
+                        this.tailCurrentPosition()
+                    }
+                }
                 break;
-}
-return {x:this.startingX,y:this.startingY}
+        }
+        return { x: this.headX, y: this.headY }
+    }
+    tailCurrentPosition() {
+        this.uniquePositions.add(`${this.tailX},${this.tailY}`)
+    }
+    uniqueTailPositions() {
+        return this.uniquePositions.size
+    }
+    getTailPosition() {
+        return { headX: this.headX, headY: this.headY, tailX: this.tailX, tailY: this.tailY }
     }
 
 }
 
 const head = new Head()
-console.log(head.move("R",2))
+function headMovement(input: string[]) {
+
+    input.forEach((movement) => {
+        const [direction, length] = movement.split(" ")
+        head.move(direction, Number(length))
+    })
+    return head.uniqueTailPositions()
+}
+
+console.log("number of unique positions: ", headMovement(lines))
