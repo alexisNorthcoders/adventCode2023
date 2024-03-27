5e30811570ef0b07e5fe7dfa3d22acb527d1ed5c
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-27 10:28:19 +0000

    add part 2 day 10 solution

--- TypeScript/2022/day10.ts
+++ TypeScript/2022/day10.ts
@@ -9,51 +9,88 @@ class CPU {
     x: number
     signalStrentghArray: number[]
     cycles: number[]
+    screen: string
+    pixel: number
     constructor(x: number = 1, cycle: number = 1) {
         this.x = x
+        this.pixel = 0
         this.cycle = cycle
         this.signalStrentghArray = []
         this.cycles = [20, 60, 100, 140, 180, 220]
+        this.screen = ""
+        this.drawPixel()
     }
 
     instruction(code: string, value: number) {
-        if (code === "noop") {
-            this.cycle++
-            if (this.cycles.includes(this.cycle)) {
-                this.signalStrentghArray.push(this.signalStrength())
-            }
+        this.cycle++
+        this.pixel++
+        this.drawPixel()
+        if (this.cycles.includes(this.cycle)) {
+            this.signalStrentghArray.push(this.signalStrength())
         }
-        else if (code === "addx") {
+        if (code === "addx") {
             this.cycle++
-            if (this.cycles.includes(this.cycle)) {
-                this.signalStrentghArray.push(this.signalStrength())
-            }
-            this.cycle++
+            this.pixel++
+
             this.x += value
+            this.drawPixel()
+
             if (this.cycles.includes(this.cycle)) {
                 this.signalStrentghArray.push(this.signalStrength())
             }
-            
         }
     }
     signalStrength() {
         return this.x * this.cycle
     }
-    sumOfSignalStrength(){
-        return this.signalStrentghArray.reduce((a, b) => a + b)
+    sumOfSignalStrength() {
+        return this.signalStrentghArray.reduce((a, b) => a + b, 0)
+    }
+    drawPixel() {
+        if (this.pixel >= 240) { 
+            // stop drawing
+        }
+        else {
+            //console.log("pixel: ",this.pixel,"X: ",this.x,"cycle: ",this.cycle)
+            const pixelPosition = this.pixel % 40
+            if (pixelPosition === 0) {
+                this.screen += "\n"
+            }
+            if (Math.abs(this.x - pixelPosition) <= 1) {
+                this.screen += "🎁";
+            }
+            else {
+                this.screen += "🎄"
+            }
+        }
+
     }
 }
 
 function sumOfSignalStrength(input: string[]) {
     const cpu = new CPU()
-    
+
     input.forEach(line => {
         const [code, value] = line.split(" ")
         cpu.instruction(code, Number(value))
-       
+
     });
-    
+
     return cpu.sumOfSignalStrength()
 }
 
+function drawImage(input: string[]) {
+    const cpu = new CPU()
+
+    input.forEach(line => {
+        const [code, value] = line.split(" ")
+        cpu.instruction(code, Number(value))
+
+
+    });
+
+    return cpu.screen
+}
+
 console.log("sum of the six signal strengths: ", sumOfSignalStrength(lines))
+console.log("CRT screen:\n", drawImage(lines))
