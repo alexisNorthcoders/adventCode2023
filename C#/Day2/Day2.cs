d44d912f9daa4a5b201ca9bf0c3d24c578594b28
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-30 15:26:13 +0000

    add solution part 2 day 2 c# 2020

--- C#/Day2/Day2.cs
+++ C#/Day2/Day2.cs
@@ -16,18 +16,19 @@ namespace AdventOfCode2020.Day2
                 char charToCheck = split[1][0];
                 string password = split[2];
                 int charCounter = 0;
-                
+
                 // int counter = password.Split(charToCheck).Length -1;
-                for (int j=0;j<password.Length;j++){
-                    if (password[j] == charToCheck){
+                for (int j = 0; j < password.Length; j++)
+                {
+                    if (password[j] == charToCheck)
+                    {
                         charCounter++;
                     }
                 }
-                if (charCounter >= min && charCounter <= max){
+                if (charCounter >= min && charCounter <= max)
+                {
                     validPasswords++;
-
                 }
-                
             }
             return validPasswords;
         }
@@ -37,7 +38,25 @@ namespace AdventOfCode2020.Day2
     {
         public static int SolvePuzzle(string[] input)
         {
-            return -1;
+            int validPasswords = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] split = input[i].Split(" ");
+                string[] minMax = split[0].Split("-");
+                int min = int.Parse(minMax[0]);
+                int max = int.Parse(minMax[1]);
+                char charToCheck = split[1][0];
+                string password = split[2];
+               
+                if (
+                    (password[min - 1] == charToCheck && password[max - 1] != charToCheck)
+                    || (password[min - 1] != charToCheck && password[max - 1] == charToCheck)
+                )
+                {
+                    validPasswords++;
+                }
+            }
+            return validPasswords;
         }
     }
 }
