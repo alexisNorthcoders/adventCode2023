2b6aec9d47cb39fabf7805a0829adcc4113c0ab0
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-30 23:26:54 +0000

    add solution for part 2 day 3 2020, change int for long because overflow issues

--- C#/Day3/Day3.cs
+++ C#/Day3/Day3.cs
@@ -24,9 +24,35 @@ namespace AdventOfCode2020.Day3
 
     public class Part2
     {
-        public static int SolvePuzzle(string[] input)
+        public static long SolvePuzzle(string[] input)
         {
-            return -1;
+            int[] slopesX = { 1, 3, 5, 7, 1 };
+            int[] slopesY = { 1, 1, 1, 1, 2 };
+            long result = 1;
+
+            for (int i = 0; i < slopesX.Length; i++)
+            {
+                result *= findTrees(input, slopesX[i], slopesY[i]);
+            }
+
+            return result;
+        }
+
+        public static long findTrees(string[] input, int positionX, int positionY)
+        {
+            long treeCounter = 0;
+            int posX = positionX;
+            int slopeLength = input[0].Length;
+            for (int i = positionY; i < input.Length; i += positionY)
+            {
+                if (input[i][posX] == '#')
+                {
+                    treeCounter++;
+                }
+                posX += positionX;
+                posX %= slopeLength;
+            }
+            return treeCounter;
         }
     }
 }
