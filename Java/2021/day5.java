780e749cf54ed13bf3590f56ba26f4794e01ce81
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-02 00:16:18 +0100

    remove unused method

--- Java/2021/day5.java
+++ Java/2021/day5.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public class day5 {
     public static void main(String[] args) {
         String filename;
-        if (args.length > 0 && args[0].equals("sample")) {
+        if (args.length > 1 && args[1].equals("sample")) {
             filename = "./inputs/sample_day5.txt";
         } else {
             filename = "./inputs/day5.txt";
@@ -29,17 +29,25 @@ public class day5 {
                 int y1 = Integer.parseInt(startPointArray[1]);
                 int x2 = Integer.parseInt(endPointArray[0]);
                 int y2 = Integer.parseInt(endPointArray[1]);
-
-                if (x1 == x2 || y1 == y2) {
+                if (args.length > 0 && args[0].equals("part2")) {
                     Point startPoint = new Point(x1, y1);
                     Point endPoint = new Point(x2, y2);
                     Segment segment = new Segment(startPoint, endPoint);
                     segments.add(segment);
                 }
+                else if (args.length > 0 && args[0].equals("part1")) {
+                    if (x1 == x2 || y1 == y2) {
+                        Point startPoint = new Point(x1, y1);
+                        Point endPoint = new Point(x2, y2);
+                        Segment segment = new Segment(startPoint, endPoint);
+                        segments.add(segment);
+                    }
+
+                }
 
             }
-            int result1 = part1(segments);
-            System.out.println("At how many points do at least two lines overlap? "+ result1);
+            int result1 = countOverlaps(segments);
+            System.out.println("At how many points do at least two lines overlap? " + result1);
 
             bufferedReader.close();
         } catch (IOException e) {
@@ -47,7 +55,7 @@ public class day5 {
         }
     }
 
-    public static int part1(List<Segment> segments) {
+    public static int countOverlaps(List<Segment> segments) {
         int countPairs = 0;
         for (Map.Entry<Point, Integer> entry : Segment.pointCounts.entrySet()) {
             if (entry.getValue() >= 2) {
@@ -65,11 +73,6 @@ public class day5 {
             this.x = x;
             this.y = y;
         }
-
-        public boolean isCollinear(Point a, Point b) {
-            return (b.y - a.y) * (x - a.x) == (y - a.y) * (b.x - a.x);
-        }
-
         @Override
         public boolean equals(Object o) {
             if (this == o)
@@ -113,18 +116,23 @@ public class day5 {
             if (directionY == 0) {
                 for (int x = startX; x != endX + directionX; x += directionX) {
                     Point point = new Point(x, startY);
-
                     pointCounts.put(point, pointCounts.getOrDefault(point, 0) + 1);
 
                 }
             } else if (directionX == 0) {
                 for (int y = startY; y != endY + directionY; y += directionY) {
                     Point point = new Point(startX, y);
-
                     pointCounts.put(point, pointCounts.getOrDefault(point, 0) + 1);
 
                 }
 
+            } else {
+                for (int x = startX; x != endX + directionX; x += directionX) {
+                    int y = startY + (x - startX) * (endY - startY) / (endX - startX);
+                    Point point = new Point(x, y);
+                    pointCounts.put(point, pointCounts.getOrDefault(point, 0) + 1);
+
+                }
             }
         }
     }
