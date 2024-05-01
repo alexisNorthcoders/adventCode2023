cca9fdd29e50c2911af8f9ad33c186542c7b8e4f
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-01 01:24:11 +0100

    add solution to part 1 day 2 in Java

--- Java/2021/day2.java
+++ Java/2021/day2.java
@@ -6,23 +6,40 @@ public class day2 {
     public static void main(String[] args) {
         String filename;
         if (args.length > 0 && args[0].equals("sample")) {
-            filename = "./inputs/sample_day2.txt"; 
+            filename = "./inputs/sample_day2.txt";
         } else {
-            filename = "./inputs/day2.txt"; 
+            filename = "./inputs/day2.txt";
         }
-        try {
-            FileReader fileReader = new FileReader(filename);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            
+        int result = calculateProduct(filename);
+        System.out.println(
+                "What do you get if you multiply your final horizontal position by your final depth? " + result);
+    }
+
+    public static int calculateProduct(String filename) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            int depth = 0;
+            int horizontal = 0;
             String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                
-                System.out.println(line);
+            while ((line = br.readLine()) != null) {
+                String[] instructions = line.split(" ");
+                switch (instructions[0]) {
+                    case "forward":
+                        horizontal += Integer.parseInt(instructions[1]);
+                        break;
+                    case "up":
+                        depth -= Integer.parseInt(instructions[1]);
+                        break;
+                    case "down":
+                        depth += Integer.parseInt(instructions[1]);
+                        break;
+                }
+
             }
-            
-            bufferedReader.close();
+            return depth * horizontal;
+
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error reading file: " + e.getMessage());
+            return 0;
         }
     }
 }
