b1cb2112806bb21fd38276752d0e847380a7f44b
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-01 10:52:42 +0100

    add solution part 1 day 3 in Java

--- Java/2021/day3.java
+++ Java/2021/day3.java
@@ -6,23 +6,53 @@ public class day3 {
     public static void main(String[] args) {
         String filename;
         if (args.length > 0 && args[0].equals("sample")) {
-            filename = "./inputs/sample_day3.txt"; 
+            filename = "./inputs/sample_day3.txt";
         } else {
-            filename = "./inputs/day3.txt"; 
+            filename = "./inputs/day3.txt";
         }
-        try {
-            FileReader fileReader = new FileReader(filename);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            
+        int result1 = part1(filename);
+        System.out.println(
+                "What do you get if you multiply your final horizontal position by your final depth? " + result1);
+    }
+
+    public static int part1(String filename) {
+        int[] sums = null;
+        int lineCounter = 0;
+        String gamma = "";
+        String epsilon = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                
-                System.out.println(line);
+            while ((line = br.readLine()) != null) {
+                lineCounter++;
+                if (lineCounter == 1) {
+                    sums = new int[line.length()];
+                }
+                for (int i = 0; i < line.length(); i++) {
+
+                    if (line.charAt(i) == '0') {
+                        sums[i]++;
+                    }
+                }
             }
-            
-            bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
+        for (int i = 0; i < sums.length; i++) {
+           
+            if (sums[i] < lineCounter/2) {
+               gamma += 0;
+               epsilon += 1;
+            }
+            else {
+                gamma += 1;
+                epsilon += 0;
+            }
+        }
+      
+        int gammaDecimal = Integer.parseInt(gamma, 2);
+        int epsilonDecimal = Integer.parseInt(epsilon, 2);
+        return gammaDecimal*epsilonDecimal;
     }
 }
