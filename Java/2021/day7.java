7aebe36258994b8e76ecc05a87df3aa973f3aaed
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-07 23:41:14 +0100

    add solution part 2 day 7 Java 2021

--- Java/2021/day7.java
+++ Java/2021/day7.java
@@ -2,16 +2,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import  java.util.stream.IntStream;
 
 public class day7 {
     public static void main(String[] args) {
         String filename;
         if (args.length > 0 && args[0].equals("sample")) {
-            filename = "./inputs/sample_day7.txt"; 
+            filename = "./inputs/sample_day7.txt";
         } else {
-            filename = "./inputs/day7.txt"; 
+            filename = "./inputs/day7.txt";
         }
         try {
             FileReader fileReader = new FileReader(filename);
@@ -20,45 +18,92 @@ public class day7 {
             bufferedReader.close();
 
             int result1 = part1(submarinePositions);
-            System.out.println("How much fuel must they spend to align to that position? "+ result1);
+            System.out.println("How much fuel must they spend to align to that position? " + result1);
+            int result2 = part2(submarinePositions);
+            System.out.println("How much fuel must they spend to align to that position? " + result2);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static int part1(String[] submarinePositions){
-        
+
+    public static int part1(String[] submarinePositions) {
+
         int max = Integer.parseInt(submarinePositions[0]);
         int min = Integer.parseInt(submarinePositions[0]);
 
         for (int i = 1; i < submarinePositions.length; i++) {
             int num = Integer.parseInt(submarinePositions[i]);
-           
+
             if (num > max) {
                 max = num;
             }
-       
+
             if (num < min) {
                 min = num;
             }
         }
         ArrayList<Integer> fuelSpent = new ArrayList<Integer>();
-        for (int j=min;j<max;j++){
+        for (int j = min; j < max; j++) {
             int totalFuel = 0;
-        for (int i=0;i<submarinePositions.length;i++){
-            totalFuel += Math.abs(Integer.parseInt(submarinePositions[i])-j);
+            for (int i = 0; i < submarinePositions.length; i++) {
+                totalFuel += Math.abs(Integer.parseInt(submarinePositions[i]) - j);
+            }
+            fuelSpent.add(totalFuel);
         }
-        fuelSpent.add(totalFuel);
-    }
-    int minimumFuel = fuelSpent.get(0);
+        int minimumFuel = fuelSpent.get(0);
 
-   
-    for (int i = 1; i < fuelSpent.size(); i++) {
-        int num = fuelSpent.get(i);
-       
-        if (num < minimumFuel) {
-            minimumFuel = num;
+        for (int i = 1; i < fuelSpent.size(); i++) {
+            int num = fuelSpent.get(i);
+
+            if (num < minimumFuel) {
+                minimumFuel = num;
+            }
         }
+        return minimumFuel;
     }
-    return minimumFuel;
-}
+
+    public static int part2(String[] submarinePositions) {
+        int max = Integer.parseInt(submarinePositions[0]);
+        int min = Integer.parseInt(submarinePositions[0]);
+
+        for (int i = 1; i < submarinePositions.length; i++) {
+            int num = Integer.parseInt(submarinePositions[i]);
+
+            if (num > max) {
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+        }
+        ArrayList<Integer> fuelSpent = new ArrayList<Integer>();
+        for (int j = min; j < max; j++) {
+            int totalFuel = 0;
+            for (int i = 0; i < submarinePositions.length; i++) {
+                totalFuel += fuelCost(Integer.parseInt(submarinePositions[i]), j);
+
+            }
+            fuelSpent.add(totalFuel);
+        }
+        int minimumFuel = fuelSpent.get(0);
+
+        for (int i = 1; i < fuelSpent.size(); i++) {
+            int num = fuelSpent.get(i);
+
+            if (num < minimumFuel) {
+                minimumFuel = num;
+            }
+        }
+        return minimumFuel;
+    }
+
+    public static int fuelCost(int initialPosition, int finalPosition) {
+        int distance = Math.abs(initialPosition - finalPosition);
+        int fuel = 0;
+        for (int i = 0; i <= distance; i++) {
+            fuel += i;
+        }
+        return fuel;
+    }
 }
