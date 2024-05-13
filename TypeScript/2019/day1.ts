71a1c66efbe317f357492f724e9025d338fe5bfc
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-13 17:08:20 +0100

    add solution part 2 day 1 TypeScript

--- TypeScript/2019/day1.ts
+++ TypeScript/2019/day1.ts
@@ -6,6 +6,15 @@ const file = Bun.file(path);
 const text: string = await file.text();
 const lines: string[] = text.split("\n");
 
+console.log(
+  "What is the sum of the fuel requirements for all of the modules on your spacecraft?: " +
+    part1(lines)
+);
+console.log(
+  "What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel?: " +
+    part2(lines)
+);
+
 function fuelRequired(mass: number): number {
   return Math.floor(mass / 3) - 2;
 }
@@ -15,9 +24,20 @@ function part1(input: string[]) {
   for (const line of input) {
     sum += fuelRequired(parseInt(line));
   }
-  return sum
+  return sum;
 }
-console.log(
-  "What is the sum of the fuel requirements for all of the modules on your spacecraft?: " +
-    part1(lines)
-);
+
+function part2(input: string[]) {
+  let sum: number = 0;
+  for (const line of input) {
+    let fuelMass = fuelRequired(parseInt(line));
+    sum += fuelMass;
+
+    while (fuelRequired(fuelMass)> 0) {
+      const tempFuel = fuelRequired(fuelMass);
+      sum += tempFuel;
+      fuelMass = tempFuel
+    }
+  }
+  return sum;
+}
