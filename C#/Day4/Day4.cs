d8f605608bf8bc103550f3ef7a695e935e0ab0b9
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-21 22:53:42 +0100

    update Passport class, delete debugging methods

--- C#/Day4/Day4.cs
+++ C#/Day4/Day4.cs
@@ -25,28 +25,28 @@ namespace AdventOfCode2020.Day4
                     switch (key)
                     {
                         case "byr":
-                            passport.Byr = value;
+                            passport.byr = value;
                             break;
                         case "iyr":
-                            passport.Iyr = value;
+                            passport.iyr = value;
                             break;
                         case "eyr":
-                            passport.Eyr = value;
+                            passport.eyr = value;
                             break;
                         case "hgt":
-                            passport.Hgt = value;
+                            passport.hgt = value;
                             break;
                         case "hcl":
-                            passport.Hcl = value;
+                            passport.hcl = value;
                             break;
                         case "ecl":
-                            passport.Ecl = value;
+                            passport.ecl = value;
                             break;
                         case "pid":
-                            passport.Pid = value;
+                            passport.pid = value;
                             break;
                         case "cid":
-                            passport.Cid = value;
+                            passport.cid = value;
                             break;
 
                     }
@@ -87,7 +87,7 @@ namespace AdventOfCode2020.Day4
                             if (int.TryParse(value, out byear))
                             {
                                 if (byear < 1920 || byear > 2002) { break; }
-                                passport.Byr = value;
+                                passport.byr = value;
                                 break;
                             }
                             break;
@@ -97,7 +97,7 @@ namespace AdventOfCode2020.Day4
                             if (int.TryParse(value, out iyear))
                             {
                                 if (iyear < 2010 || iyear > 2020) { break; }
-                                passport.Iyr = value;
+                                passport.iyr = value;
                                 break;
                             }
                             break;
@@ -107,7 +107,7 @@ namespace AdventOfCode2020.Day4
                             if (int.TryParse(value, out year))
                             {
                                 if (year < 2020 || year > 2030) { break; }
-                                passport.Eyr = value;
+                                passport.eyr = value;
                                 break;
                             }
                             break;
@@ -116,30 +116,30 @@ namespace AdventOfCode2020.Day4
                             {
                                 int height = int.Parse(value.Split("cm")[0]);
                                 if (height < 150 || height > 193) { break; }
-                                passport.Hgt = value;
+                                passport.hgt = value;
                             }
                             else if (value.Contains("in"))
                             {
                                 int height = int.Parse(value.Split("in")[0]);
                                 if (height < 59 || height > 76) { break; }
-                                passport.Hgt = value;
+                                passport.hgt = value;
                             }
 
                             break;
                         case "hcl":
                             if (!value.Contains('#') || value.Length != 7) { break; }
-                            passport.Hcl = value;
+                            passport.hcl = value;
                             break;
                         case "ecl":
                             if (!validColors.Contains(value)) { break; }
-                            passport.Ecl = value;
+                            passport.ecl = value;
                             break;
                         case "pid":
                             if (value.Length != 9) { break; }
-                            passport.Pid = value;
+                            passport.pid = value;
                             break;
                         case "cid":
-                            passport.Cid = value;
+                            passport.cid = value;
                             break;
 
                     }
@@ -153,14 +153,14 @@ namespace AdventOfCode2020.Day4
     }
     class Passport
     {
-        private string byr;
-        private string iyr;
-        private string eyr;
-        private string hgt;
-        private string hcl;
-        private string ecl;
-        private string pid;
-        private string? cid;
+        public string byr;
+        public string iyr;
+        public string eyr;
+        public string hgt;
+        public string hcl;
+        public string ecl;
+        public string pid;
+        public string? cid;
 
         public Passport()
         {
@@ -181,51 +181,6 @@ namespace AdventOfCode2020.Day4
             }
             return true;
         }
-        public void StringPassport()
-        {
-            Console.WriteLine($"byr:{byr} iyr:{iyr} eyr:{eyr} hgt:{hgt} hcl:{hcl} ecl:{ecl} pid:{pid}");
-        }
-
-        public string? Cid
-        {
-            get => cid;
-            set => cid = value;
-        }
-        public string Ecl
-        {
-            get => ecl;
-            set => ecl = value;
-        }
-        public string Byr
-        {
-            get => byr;
-            set => byr = value;
-        }
-        public string Iyr
-        {
-            get => iyr;
-            set => iyr = value;
-        }
-        public string Eyr
-        {
-            get => eyr;
-            set => eyr = value;
-        }
-        public string Hgt
-        {
-            get => hgt;
-            set => hgt = value;
-        }
-        public string Hcl
-        {
-            get => hcl;
-            set => hcl = value;
-        }
-        public string Pid
-        {
-            get => pid;
-            set => pid = value;
-        }
-
+       
     }
 }
