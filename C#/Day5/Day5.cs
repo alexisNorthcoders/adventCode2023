87ef8dd254183cbdcba4ed6e392c4b29cefc5198
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-21 22:57:22 +0100

    add boilerplate files for Day5

---
+++ C#/Day5/Day5.cs
@@ -0,0 +1,16 @@
+
+namespace AdventOfCode2020.Day5
+{
+    public class Part1
+    {
+        public static int SolvePuzzle(string[] input)
+        {
+            return 0;
+        }
+    }
+    public class Part2{
+        public static int SolvePuzzle(string[] input){
+            return 0;
+        }
+    }
+}
