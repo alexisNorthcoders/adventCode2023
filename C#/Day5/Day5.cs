40d63f5036db2b2ccb7c7b752596a5bdbc27b664
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-05-22 01:07:56 +0100

    add solution for part 1 day 5 2020 C#

--- C#/Day5/Day5.cs
+++ C#/Day5/Day5.cs
@@ -5,11 +5,56 @@ namespace AdventOfCode2020.Day5
     {
         public static int SolvePuzzle(string[] input)
         {
-            return 0;
+            double seatID = 0;
+            foreach (string line in input)
+            {
+                double fbStart = 0;
+                double fbEnd = 127;
+                double lrStart = 0;
+                double lrEnd = 7;
+                double row = 0;
+                double column = 0;
+                
+                for (int i = 0; i < line.Length; i++)
+                {
+                    
+                    if (i == 6)
+                    {
+                        row = line[i] == 'F' ? Math.Min(fbEnd, fbStart) : Math.Max(fbEnd, fbStart);
+                    }
+                    if (i == 9)
+                    {
+                        column = line[i] == 'L' ? Math.Min(lrEnd, lrStart) : Math.Max(lrEnd, lrStart);
+                    }
+                    if (line[i] == 'F')
+                    {
+                        fbEnd -= Math.Ceiling((fbEnd - fbStart) / 2);
+                    }
+                    if (line[i] == 'B')
+                    {
+                        fbStart += Math.Ceiling((fbEnd - fbStart) / 2);
+                    }
+                    if (line[i] == 'L')
+                    {
+                        lrEnd -= Math.Ceiling((lrEnd - lrStart) / 2);
+
+                    }
+                    if (line[i] == 'R')
+                    {
+                        lrStart += Math.Ceiling((lrEnd - lrStart) / 2);
+                    }
+
+                }
+                seatID = Math.Max(seatID,row*8+column);
+              
+            }
+            return Convert.ToInt32(seatID);
         }
     }
-    public class Part2{
-        public static int SolvePuzzle(string[] input){
+    public class Part2
+    {
+        public static int SolvePuzzle(string[] input)
+        {
             return 0;
         }
     }
