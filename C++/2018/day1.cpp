dbdd9537a251271092b3dd2893a08acea86f94f5
Alexis <alexiscreoulo@hotmail.com>
2024-08-07 15:33:41 +0100

    split main into 2 parts, add solution for part 2 day 1.

--- C++/2018/day1.cpp
+++ C++/2018/day1.cpp
@@ -1,24 +1,82 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
+
+int firstPart(const string &filePath)
+{
+    ifstream inputFile(filePath);
+    if (!inputFile.is_open())
+    {
+        cerr << "Unable to open file: " << filePath << endl;
+        return -1;
+    }
+
+    string line;
+    int frequency = 0;
+    while (getline(inputFile, line))
+    {
+        frequency += stoi(line);
+    }
+    inputFile.close();
+    return frequency;
+}
+
+int secondPart(const string &filePath)
+{
+
+    int result = 0;
+    int frequency = 0;
+
+    vector<int> numbers;
+    bool duplicateFound = false;
+    numbers.push_back(frequency);
+    while (!duplicateFound)
+    {
+        string line;
+        ifstream inputFile(filePath);
+        if (!inputFile.is_open())
+        {
+            cerr << "Unable to open file: " << filePath << endl;
+            return -1;
+        }
+        while (getline(inputFile, line))
+        {
+            frequency += stoi(line);
+
+            for (int i = 0; i < numbers.size(); i++)
+            {
+                if (frequency == numbers[i])
+                {
+                    duplicateFound = true;
+                    return frequency;
+                }
+            }
+            numbers.push_back(frequency);
+        }
+
+        inputFile.close();
+    }
+
+    return 0;
+}
+
 int main()
 {
     string filePath = "./inputs/day1.txt";
 
-    ifstream inputFile(filePath);
-    if (!inputFile.is_open())
+    int resultPart1 = firstPart(filePath);
+    if (resultPart1 != -1)
     {
-        std::cerr << "Unable to open file: " << filePath << std::endl;
-        return 1;
+        cout << "Day 1 part 1: " << resultPart1 << endl;
     }
-    string line;
-    int number = 0;
-    while (getline(inputFile, line))
+
+    int resultPart2 = secondPart(filePath);
+    if (resultPart2 != -1)
     {
-        number += stoi(line);
-        }
-    inputFile.close();
-    cout << "Day 1 part 1: " << number << endl;
+        cout << "Day 1 part 2: " << resultPart2 << endl;
+    }
+
     return 0;
 }
