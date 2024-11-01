d3c7baa14c12e65130d33e70cd04eaafa3b6952d
Alexis <alexiscreoulo@hotmail.com>
2024-11-01 22:19:45 +0000

    2017 | Day 2 | Part 1 | Go - add solution

---
+++ Go/2017/day2/day2.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Day 2\n Part 1:", part1())
+	fmt.Println(" Part 2:", part2())
+}
+func readFile(filePath string) [][]int {
+	var fileContent [][]int
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var lineContent []int
+		fields := strings.Fields(line)
+		for _, field := range fields {
+			num, err := strconv.Atoi(field)
+			if err != nil {
+				fmt.Println("Error converting string to integer:", err)
+				return nil
+			}
+			lineContent = append(lineContent, num)
+		}
+
+		fileContent = append(fileContent, lineContent)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	return fileContent
+}
+func part1() int {
+	filePath := "./input/day2.txt"
+
+	var checksum int
+
+	var fileContent = readFile(filePath)
+
+	for i, line := range fileContent {
+		var minimum int = fileContent[i][0]
+		var maximum int = fileContent[i][0]
+		for _, v := range line {
+			if v > maximum {
+				maximum = v
+			}
+			if v < minimum {
+				minimum = v
+			}
+		}
+		checksum += maximum - minimum
+	}
+
+	return checksum
+}
+
+func part2() int {
+	filePath := "./input/sample_day2.txt"
+
+	var fileContent = readFile(filePath)
+	fmt.Println(fileContent)
+
+	return 0
+}
