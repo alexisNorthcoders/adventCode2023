13b165b8f633f8af177ae1bf2af0f4526d81d50f
Alexis <alexiscreoulo@hotmail.com>
2024-11-16 11:00:39 +0000

    2018 | Day 1 | Part 2 | Clojure

--- Clojure/2018/aoc-2018/src/aoc_2018/day1.clj
+++ Clojure/2018/aoc-2018/src/aoc_2018/day1.clj
@@ -1,15 +1,32 @@
 (ns aoc-2018.day1
   (:require
+   [aoc-2018.day1 :as day1]
    [clojure.string :as str]))
 
 (defn read-input []
   (slurp "resources/day1.txt"))
 
-(defn solve []
-  (let [input (read-input)
-        numbers (map #(Integer/parseInt %) (str/split input #"\n"))
+(defn parse-input []
+  (map #(Integer/parseInt %) (str/split (read-input) #"\n")))
+
+(defn part1 []
+  (let [numbers (parse-input)
         total-sum (reduce + numbers)]
+    total-sum))
 
+(defn part2 []
+  (loop [seen #{}
+         current-sum 0
+         remaining (cycle (parse-input))]
+    (let [next-number (first remaining)
+          new-sum (+ current-sum next-number)]
+      (if (seen new-sum)
+        new-sum
+        (recur (conj seen new-sum) new-sum (rest remaining))))))
 
-    (println "Day 1: Part 1:")
-    (println total-sum)))
+(defn solve []
+  (println "Day 1: Part 1:")
+  (println (day1/part1))
+
+  (println "Day 1: Part 2:")
+  (println (day1/part2)))
