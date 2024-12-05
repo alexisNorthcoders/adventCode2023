53a5622abf43e3597cbc62367c5cbdd9da096f95
Alexis <alexiscreoulo@hotmail.com>
2024-12-05 22:19:44 +0000

    refactor to make functions reusable and memioze the results

--- JavaScript/2024/day5.js
+++ JavaScript/2024/day5.js
@@ -21,27 +21,81 @@ async function day5() {
 
 function part1(lines) {
 
-    const pageOrders = {}
-    const pageUpdates = []
-    let i = 0
-
-    while (!lines[i].match(/^$/)) {
-        const [left, right] = lines[i].split('|')
-        pageOrders[left] = pageOrders[left] ? [...pageOrders[left], (Number(right))] : [Number(right)]
-        i++
-    }
-    while (i < lines.length) {
-        if (lines[i].match(/^$/)) {
-            i++
-            continue
-        }
-        pageUpdates.push(lines[i].split(',').map(Number))
-        i++
-    }
+    const { pageOrders, pageUpdates } = parseInput(lines)
 
     const validUpdates = []
+
     pageUpdates.forEach(update => {
 
+        if (checkValidUpdates(update, pageOrders)) validUpdates.push(update[Math.floor(update.length / 2)])
+    })
+
+    return validUpdates.reduce((a, b) => a + b)
+}
+
+function part2(lines) {
+    const { pageOrders, pageUpdates } = parseInput(lines)
+
+    const invalidUpdatesIndexes = []
+
+    pageUpdates.forEach((update,i) => {
+
+        if (!checkValidUpdates(update, pageOrders)) invalidUpdatesIndexes.push(i)
+    })
+
+    console.log(invalidUpdatesIndexes)
+
+
+}
+
+
+day5()
+
+const parseInput = (() => {
+    const cache = new Map();
+    return (lines) => {
+        const cacheKey = lines.length;
+
+        if (cache.has(cacheKey)) {
+            return cache.get(cacheKey);
+        }
+
+        const pageOrders = {};
+        const pageUpdates = [];
+        let i = 0;
+
+        while (!lines[i].match(/^$/)) {
+            const [left, right] = lines[i].split('|');
+            pageOrders[left] = pageOrders[left]
+                ? [...pageOrders[left], Number(right)]
+                : [Number(right)];
+            i++;
+        }
+
+        while (i < lines.length) {
+            if (lines[i].match(/^$/)) {
+                i++;
+                continue;
+            }
+            pageUpdates.push(lines[i].split(',').map(Number));
+            i++;
+        }
+
+        const result = { pageOrders, pageUpdates };
+        cache.set(cacheKey, result);
+        return result;
+    };
+})();
+
+const checkValidUpdates = (() => {
+
+    const cache = new Map();
+    return (update, pageOrders) => {
+        const cacheKey = JSON.stringify(update);
+
+        if (cache.has(cacheKey)) {
+            return cache.get(cacheKey);
+        }
         let validUpdate = true
         for (let i = 0; i < update.length; i++) {
             for (let j = i + 1; j < update.length; j++) {
@@ -57,14 +111,8 @@ function part1(lines) {
                 }
             }
         }
-        if (validUpdate) validUpdates.push(update[Math.floor(update.length / 2)])
-    })
-    return validUpdates.reduce((a, b) => a + b)
-
-}
-
-function part2() {
-
-}
-
-day5()
+        const result = validUpdate
+        cache.set(cacheKey, result);
+        return result;
+    };
+})();
