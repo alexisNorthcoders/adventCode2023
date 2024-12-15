8f8a4b929911999dc0cc59e2ef153fc63cab9255
Alexis <alexiscreoulo@hotmail.com>
2024-12-15 18:47:06 +0000

    add solution part 1 day 15 2024 JavaScript

--- JavaScript/2024/day15.js
+++ JavaScript/2024/day15.js
@@ -11,23 +11,209 @@ async function day15() {
     console.log('🌟 --- Day 15 Results --- 🌟');
     console.time('Total Time');
     console.time('Parse Input');
-    const robots = extractNumbers(file)
+    const { wallsCoords, robotCoords, boxesCoords, movements } = parseText(lines)
     console.timeEnd('Parse Input');
     console.time('T1');
-    console.log('📌 Part 1: ', part1(lines));
+    console.log('📌 Part 1: ', part1({ wallsCoords, robotCoords, boxesCoords, movements }));
     console.timeEnd('T1');
     console.time('T2');
-    console.log('📌 Part 2: ', part2(lines));
+    console.log('📌 Part 2: ', part2({ wallsCoords, robotCoords, boxesCoords, movements }));
     console.timeEnd('T2');
     console.timeEnd('Total Time');
 }
 
-function part1() {
+let boxes = []
+let walls = []
+let size = 1
 
+function part1({ wallsCoords, robotCoords, boxesCoords, movements }) {
+
+    boxes = boxesCoords.map(box => new Box(box.x, box.y, size));
+    const robot = new Robot(robotCoords.x, robotCoords.y, size);
+    walls = wallsCoords.map((wall) => new Wall(wall.x, wall.y, size))
+
+    movements.forEach((movement) => {
+        switch (movement) {
+            case ">":
+                robot.move('right');
+                break;
+            case "<":
+                robot.move('left');
+                break;
+            case "^":
+                robot.move('up');
+                break;
+            case "v":
+                robot.move('down');
+                break;
+        }
+    })
+
+    return boxes.reduce((a, b) => a + b.x + 100 * b.y, 0)
 }
 
-function part2() {
+function part2({ wallsCoords, robotCoords, boxesCoords, movements }) {
 
 }
 
 day15()
+
+function parseText(lines) {
+
+    let robotCoords
+    const wallsCoords = []
+    const boxesCoords = []
+    const movements = []
+
+    let row = 0
+
+    while (lines[row] !== '') {
+        lines[row].split('').forEach((element, i) => {
+
+            if (element === '#') {
+                wallsCoords.push({ x: i, y: row })
+            }
+            if (element === "@") {
+                robotCoords = { x: i, y: row }
+            }
+            if (element === "O") {
+                boxesCoords.push({ x: i, y: row })
+            }
+        })
+        row++
+    }
+    for (row; row < lines.length; row++) {
+        movements.push(...lines[row].split(''))
+    }
+    return { wallsCoords, robotCoords, boxesCoords, movements }
+}
+
+class Robot {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    move(direction) {
+
+        let newX = this.x;
+        let newY = this.y;
+
+        switch (direction) {
+            case 'up':
+                newY -= size;
+                break;
+            case 'down':
+                newY += size;
+                break;
+            case 'left':
+                newX -= size;
+                break;
+            case 'right':
+                newX += size;
+                break;
+        }
+
+        if (!this.checkCollision(newX, newY, direction)) {
+            this.x = newX;
+            this.y = newY;
+        }
+    }
+    checkCollision(newX, newY, direction) {
+
+        for (const wall of walls) {
+            if (
+                newX < wall.x + wall.size &&
+                newX + this.size > wall.x &&
+                newY < wall.y + wall.size &&
+                newY + this.size > wall.y
+            ) {
+                return true;
+            }
+        }
+        for (const box of boxes) {
+            if (
+                newX < box.x + box.size &&
+                newX + this.size > box.x &&
+                newY < box.y + box.size &&
+                newY + this.size > box.y
+            ) {
+                return box.move(direction)
+            }
+        }
+        return false;
+    }
+}
+
+class Box {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+
+    }
+    move(direction) {
+
+        let newX = this.x;
+        let newY = this.y;
+
+        switch (direction) {
+            case 'up':
+                newY -= size;
+                break;
+            case 'down':
+                newY += size;
+                break;
+            case 'left':
+                newX -= size;
+                break;
+            case 'right':
+                newX += size;
+                break;
+        }
+
+        if (!this.checkCollision(newX, newY, direction)) {
+            this.x = newX;
+            this.y = newY;
+            return false
+        }
+        return true
+    }
+    checkCollision(newX, newY, direction) {
+
+        for (const wall of walls) {
+            if (
+                newX < wall.x + wall.size &&
+                newX + this.size > wall.x &&
+                newY < wall.y + wall.size &&
+                newY + this.size > wall.y
+            ) {
+                return true;
+            }
+        }
+        for (const box of boxes) {
+            if (
+                newX < box.x + box.size &&
+                newX + this.size > box.x &&
+                newY < box.y + box.size &&
+                newY + this.size > box.y
+            ) {
+                return box.move(direction)
+            }
+        }
+        return false;
+    }
+}
+
+class Wall {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+}
+
+
+
+
